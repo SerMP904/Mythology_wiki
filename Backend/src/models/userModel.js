b0874193486555662655bf38f0ce218b05e9d8d3
sergioM <sermarpad@gmail.com>
2025-12-28 12:43:15 +0100

    created models for users and myths

---
+++ Backend/src/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    name:{
+        type: String,
+        required: [true, "El nombre es obligatorio"],
+        minlength: [3, "El nombre debe tener al menos 3 caracteres"],
+        maxlength: 30,
+    },
+    username:{
+        type: String,
+        required: [true, "El nombre de usuario es obligatorio"],
+        minlength: [3, "El nombre de usuario debe tener al menos 3 caracteres"],
+        maxlength: 30,
+    },
+    email:{
+        type: String,
+        required: [true, "El correo es obligatorio"],
+        unique: [true, "El email introducido ya existe"],
+    },
+    password:{
+        type: String,
+        required: [true, "La contraseña es obligatoria"],
+        unique: [true, "El email introducido ya existe"],
+    },
+    profilePicture:{
+        type: String,
+    },
+    role:{
+        type: String,
+        enum: [user, admin],
+        default: "user",
+    },
+    isActive:{
+        type: Boolean,
+        default: true,
+    },
+})
+
+const userModel = mongoose.model("User", userSchema, "users");
+
+module.exports = userModel;
