240f9c6b46d902cdcf06f10e39e9a81647f5cb6a
sergioM <sermarpad@gmail.com>
2025-12-29 11:37:17 +0100

    created .env, middleware.js, authRouter and userRouter

---
+++ Backend/src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const token = req.header("auth-token");
+    if (!token) return res.status(401).send("Acceso denegado");
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_TOKEN);
+        req.payload = payload;
+        next();
+    } catch (error) {
+        try {
+            const payload = jwt.verify(token, process.env.SECRET_TOKEN_REFRESH)
+            req.payload = payload;
+        next();
+        } catch (error) {
+            res.status(401).send({ status: "Token expired", error: error.message });
+        }
+    }
+}
+
+module.exports = {verifyToken};
