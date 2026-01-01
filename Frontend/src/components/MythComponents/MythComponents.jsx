d6f38c8991ae884b3fede224e35d575b2272f68c
sergioM <sermarpad@gmail.com>
2026-01-01 11:57:37 +0100

    Added Myth Components, MytH Component Action, Reducer and .jsx.  Added store and index.js.

---
+++ Frontend/src/components/MythComponents/MythComponents.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
+const MythComponents = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MythComponents
