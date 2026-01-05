9c70c57dfaa4b87930dde45cc76196d32b2872d5
sergioM <sermarpad@gmail.com>
2026-01-05 19:09:35 +0100

    Updated mythModel from majorGods to majorGod. Updated mythController accordingly with last change. Created classes for divs in MythComponents and App.css

--- Frontend/src/components/MythComponents/MythComponents.jsx
+++ Frontend/src/components/MythComponents/MythComponents.jsx
@@ -24,14 +24,83 @@ const MythComponents = () => {
         <h2>cargando datos</h2>
       ) : (
         <>
+        <div className="total-myth-grid">
           {myths.map((m, idx) => {
-            return(
-            <div key={idx}>
-              <p>
-                {m.pantheon}, {m.manuscript}, {m.majorGods[0].name} y {m.monsters[0].name}: {m.monsters[0].description}
-              </p>
-            </div>);
+            return (
+              <div key={idx} className="pantheon-display">
+                <div>
+                  <p>Panteón: {m.pantheon}</p>
+                </div>
+                <div>
+                  <p>Dios Mayor: {m.majorGod.name}</p>
+                  {m.majorGod.symbols.length > 0 && (
+                    <>
+                    <p>Símbolos: </p>
+                    {m.majorGod.symbols.map((s, majorIdx) => {
+                      return (
+                        <div key={majorIdx} className="symbols-display">
+                          <p> {s} {idx < m.majorGod.symbols.length - 1 && ", "} </p>
+                        </div>
+                      );
+                    })}
+                    </>
+                    )}
+                  <div>
+                    <p>{m.majorGod.description}</p>
+                  </div>
+                </div>
+                <div>
+                  {m.otherGods.length > 0 && (
+                    <>
+                    <p>Otros dioses: </p>
+                    {m.otherGods.map((god, idx) => {
+                      return (
+                        <div key={idx} className="other-gods-display">
+                          <p>{god.name}: </p>
+                          {god.symbols.length > 0 && (
+                            <>
+                            <div>
+                            <p>Simbolos:</p>
+                            {god.symbols.map((s, godIdx) => {
+                              return (
+                                <div key={godIdx}>
+                                  <p>{s} {godIdx < god.symbols.length - 1 && ", "}</p>
+                                </div>
+                              )
+                            })}
+                            </div>
+                            </>
+                          )}
+                          <div>
+                            <p>{god.description} </p>
+                          </div>
+                        </div>
+                      )
+                    })}
+                    <div>
+                      <p>Manuscrito principal: {m.manuscript}</p>
+                    </div>
+                    <div>
+                      {m.monsters.length > 0 && (
+                        <>
+                        <p>Monstruos: </p>
+                        {m.monsters.map((monster, monsterIdx) => {
+                          return (
+                            <div key={monsterIdx}>
+                              <p>{monster.name}: {monster.description}</p>
+                            </div>
+                          )
+                        })}
+                        </>
+                      )}
+                    </div>
+                    </>
+                  )}
+                </div>
+              </div>
+            );
           })}
+          </div>
         </>
       )}
     </div>
