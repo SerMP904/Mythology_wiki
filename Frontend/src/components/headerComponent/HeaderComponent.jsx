15b4fb41f8919d2344c2ac102fd23c7c6246505e
sergioM <sermarpad@gmail.com>
2026-01-09 17:12:37 +0100

    Added a select in HeaderComponent.jsx

--- Frontend/src/components/headerComponent/HeaderComponent.jsx
+++ Frontend/src/components/headerComponent/HeaderComponent.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 
 const HeaderComponent = () => {
     const navigate = useNavigate()
     const { myths } = useSelector((state) => state.mythComponentReducer);
+    const [selectedPantheon, setSelectedPantheon] = useState('');
 
     const returnHome = () => {
         navigate("/wiki")
     }
+
+    const handlePantheonChange = (e) => {
+    const pantheon = e.target.value;
+    setSelectedPantheon(pantheon);
+    if (pantheon) {
+        navigate(`/wiki/${pantheon}`);
+      } else {
+        navigate("/wiki")
+      }
+    }
+
   return (
     <div className="header-main">
       <div className="header-div" onClick={() => returnHome()}>
@@ -17,7 +31,14 @@ const HeaderComponent = () => {
         <p>Contact</p>
       </div>
       <div className="header-div">
-        <p>Barra de búsqueda</p>
+        <select value={selectedPantheon} onChange={handlePantheonChange}>
+          <option value="">Ninguno</option>
+          {myths.map((myth, index) => (
+            <option key={index} value={myth.pantheon}>
+              {myth.pantheon}
+            </option>
+          ))}
+          </select>
       </div>
     </div>
   )
