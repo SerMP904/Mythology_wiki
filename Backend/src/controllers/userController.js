a04544f661e22fa4e64e8c0598b4a0437730bba3
sergioM <sermarpad@gmail.com>
2026-01-11 19:13:33 +0100

    Added getTokens in authController. Added several functions in userController. Added verifyAdmin and verifyUserPermissions in auth.js. Added references for the userController and auth.js new functions in userRouter.

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -13,4 +13,46 @@ const getAllUsers = async (req, res) => {
 }
 };
 
-module.exports = { getAllUsers };
+const getUserById = async (req, res) => {
+  try {
+    const { idUser } = req.params;
+    console.log(idUser);
+    const user = await userModel.findById(idUser);
+    if (!user) return res.status(200).send("No existe usuario con ese id");
+    res.status(200).send({ status: "Success", data: user });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  try {
+    const { idUser } = req.params;
+    console.log(idUser);
+    const user = await userModel.findByIdAndDelete(idUser);
+    if (!user) return res.status(200).send("No existe usuario con ese id");
+    res
+      .status(200)
+      .send({ status: "Success", message: "Usuario eliminado correctamente" });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
+const editUserById = async (req, res) => {
+  try {
+    const { idUser } = req.params;
+    const newUser = req.body;
+    const updatedUser = await userModel.findByIdAndUpdate(idUser, newUser, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedUser)
+      return res.status(200).send("No existe usuario con ese id");
+    res.status(200).send({ status: "Success", data: updatedUser });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
+module.exports = { getAllUsers, getUserById, deleteUserById, editUserById };
--- Backend/src/routers/userRouter.js
+++ Backend/src/routers/userRouter.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAllUsers
+    getAllUsers, getUserById, deleteUserById, editUserById
 } = require("../controllers/userController")
 
-const { verifyToken } = require("../middlewares/auth")
+const { verifyToken, verifyAdmin, verifyUserPermissions } = require("../middlewares/auth")
 
-router.get("/", verifyToken, getAllUsers);
+router.get("/", verifyToken, verifyAdmin, getAllUsers);
+router.get("/:idUser", getUserById);
+router.patch("/edit/:idUser", verifyToken, verifyUserPermissions, editUserById);
+router.delete("/delete/:idUser", verifyToken, verifyUserPermissions, deleteUserById);
 
 module.exports = router;
