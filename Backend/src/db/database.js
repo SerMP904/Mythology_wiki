ebde8b866281e7b79a6336f548b26607553b5ae6
sergioM <sermarpad@gmail.com>
2026-02-11 18:13:49 +0100

    Moved nodemon to devDependencies. Improved response of getAllMyths. Updated UserModel. Updated database.js. Improved userComponentAction. Removed console log in mythFetch.js. Updated getUsers in userFetch

--- Backend/src/db/database.js
+++ Backend/src/db/database.js
@@ -4,6 +4,7 @@ const connectToDatabase = async () => {
     try {
         const URL_MONGO = process.env.URL_MONGO;
         await mongoose.connect(URL_MONGO)
+        console.log("Base de datos conectada correctamente")
     } catch (error) {
         res.status(500).json({ error: 'Failed to connect to the database', message: error.message });
     }
